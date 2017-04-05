9cb216fff68b47f0569fdabc185e78f2842eeadb
Evette Cheung <FrancFranc@gmx.com>
2017-04-05 15:32:21 -0700

    Re-do Day2-Assignment

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
-var q1 = confirm ('Guess who I am in the class!');
+var q1 = confirm ('Guess who is applying for this job!');
 
 var q2 = prompt ('Am I female?').toUpperCase();
 
   if (q2=='YES')
     {
-      alert ('Yeah! You are right. I am one of the 5 female in class.');
+      alert ("Yeah! You are right. I'm a young female.");
     }
   else
     {
       alert ("Sorry, I'm not a male.")
     }
 
-var q3 = prompt ('Do I have blonde hair?').toUpperCase();
+var q3 = prompt ('Do you think I have blonde hair?').toUpperCase();
 
   if (q3=='YES')
     {
-      alert ("Well.. my hair is not in blonde.");
+      alert ("Well.. my hair is not in blonde. I have my beautiful dark brown hair.");
     }
   else
     {
-      alert ("Yes!  I have my beautiful dark brown hair.")
+      alert ("Correct! My hair isn't blonde in color.")
     }
 
 var q4 = prompt ('Do I speak any language other than English?').toUpperCase();
 
   if (q4=='YES')
     {
-      alert ('You are right. I also speak Cantonese and Mandarin. The answer of who I am is getting close.');
+      alert ('You are right. I also speak Cantonese and Mandarin. I guess you now have an idea what my nationality is.');
     }
   else
     {
       alert ("In fact, I do speak other languages.")
     }
 
-var q5 = prompt ('Do I sit at the 3rd row in class?').toUpperCase();
+var q5 = prompt ('Am I an asian?').toUpperCase();
 
   if (q5=='YES')
     {
-      alert ('Yay! The only female who sits in the 3rd row is me!');
+      alert ('Yes! You got it!');
     }
   else
     {
       alert ("Sorry, wrong guess!");
     }
 
-var q6 = prompt ('Am I an asian?').toUpperCase();
+var q6 = prompt ('Can I have an interview with you?').toUpperCase();
 
   if (q6=='YES')
     {
-      alert ("And yes, I am Evette!");
+      alert ("Thank you for offering me this chance!! My contact is written at the bottom of this page. I'm awaiting your call/email.");
     }
   else
     {
-      alert ("Hope you can guess the right one next time if I still have to write this game.");
+      alert ("I hope you can re-consider my application after seeing the hardwork I've done on writing this page.");
     };
 
 console.log(q1);
