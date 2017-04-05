c3ba3e510eee26da0be24f8620a1979d90583d76
Evette Cheung <FrancFranc@gmx.com>
2017-04-05 07:02:25 -0700

    Assignment Done.

---
+++ app.js
@@ -0,0 +1,63 @@
+var q1 = confirm ('Guess who I am in the class!');
+
+var q2 = prompt ('Am I female?').toUpperCase();
+
+  if (q2=='YES')
+    {
+      alert ('Yeah! You are right. I am one of the 5 female in class.');
+    }
+  else
+    {
+      alert ("Sorry, I'm not a male.")
+    }
+
+var q3 = prompt ('Do I have blonde hair?').toUpperCase();
+
+  if (q3=='YES')
+    {
+      alert ("Well.. my hair is not in blonde.");
+    }
+  else
+    {
+      alert ("Yes!  I have my beautiful dark brown hair.")
+    }
+
+var q4 = prompt ('Do I speak any language other than English?').toUpperCase();
+
+  if (q4=='YES')
+    {
+      alert ('You are right. I also speak Cantonese and Mandarin. The answer of who I am is getting close.');
+    }
+  else
+    {
+      alert ("In fact, I do speak other languages.")
+    }
+
+var q5 = prompt ('Do I sit at the 3rd row in class?').toUpperCase();
+
+  if (q5=='YES')
+    {
+      alert ('Yay! The only female who sits in the 3rd row is me!');
+    }
+  else
+    {
+      alert ("Sorry, wrong guess!");
+    }
+
+var q6 = prompt ('Am I an asian?').toUpperCase();
+
+  if (q6=='YES')
+    {
+      alert ("And yes, I am Evette!");
+    }
+  else
+    {
+      alert ("Hope you can guess the right one next time if I still have to write this game.");
+    };
+
+console.log(q1);
+console.log(q2);
+console.log(q3);
+console.log(q4);
+console.log(q5);
+console.log(q6);
